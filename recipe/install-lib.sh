df90fea60507da3f5eb08ff96fc9bba2a27e3d19
vkhomits <khomitsvalia@gmail.com>
2025-11-25 14:09:10 +0200

    Link cadiback and cadical staticaly

--- recipe/install-lib.sh
+++ recipe/install-lib.sh
@@ -3,24 +3,11 @@ pushd cadical
     export LDFLAGS="$LDFLAGS -Wl,-headerpad_max_install_names"
     ./configure
     make -j8
-    if [ -f build/libcadical.so ]; then
-        cp build/libcadical.so $PREFIX/lib/
-    fi
-    if [ -f build/libcadical.dylib ]; then
-        cp build/libcadical.dylib $PREFIX/lib/
-    fi
-
-    # Copy headers
-    cp src/cadical.hpp $PREFIX/include/
-    cp src/ccadical.h $PREFIX/include/
-    cp src/ipasir.h $PREFIX/include/
 popd
 
 pushd cadiback
     ./configure
     make -j8
-    cp libcadiback.so $PREFIX/lib/
-	cp cadiback.h $PREFIX/include/
 popd
 
 pushd cryptominisat
